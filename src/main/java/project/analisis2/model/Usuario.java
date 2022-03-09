c65169c34d93c3177263e1812e911f631109bf4d
jmateo95 <201313852@ingenieria.usac.edu.gt>
2022-03-09 00:14:45 -0600

    first commit

--- src/main/java/project/analisis2/model/Usuario.java
+++ src/main/java/project/analisis2/model/Usuario.java
@@ -5,8 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
 
 import java.io.Serializable;
+@Data
 @Entity
 public class Usuario implements Serializable{
     @Id
@@ -52,11 +54,11 @@ public class Usuario implements Serializable{
         this.name = name;
     }
 
-    public String getusuario() {
+    public String getUsuario() {
         return usuario;
     }
 
-    public void setusuario(String usuario) {
+    public void setUsuario(String usuario) {
         this.usuario = usuario;
     }
 
